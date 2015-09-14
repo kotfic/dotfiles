05ea07089bcc204f66d0424230590c527d3b77c6
Christopher Kotfila <chris.kotfila@kitware.com>
2015-09-14 08:55:22 -0400

    Adding global git ignore

--- movein.sh
+++ movein.sh
@@ -6,18 +6,18 @@ DOTDIR=~/.dot
 mklink(){
     if [ ! -f $1 ];
     then
-	echo "Linking $1 => $DOTDIR/$(basename $1)"
-	ln -s $DOTDIR/$(basename $1) $1
+        echo "Linking $1 => $DOTDIR/$(basename $1)"
+        ln -s $DOTDIR/$(basename $1) $1
     else
-	if [ ! -h $1 ]
-	then
-	    echo "Backing up then linking $1 to $DOTDIR/$(basename $1)"
-	    mv $1 $1.bk
-	    ln -s $DOTDIR/$(basename $1) $1
-	else
-	    echo "Skipping $1, already a Symlink"
-	fi
-    fi   
+        if [ ! -h $1 ]
+        then
+            echo "Backing up then linking $1 to $DOTDIR/$(basename $1)"
+            mv $1 $1.bk
+            ln -s $DOTDIR/$(basename $1) $1
+        else
+            echo "Skipping $1, already a Symlink"
+        fi
+    fi
 }
 
 cd $DOTDIR && git pull && cd $PWD
@@ -26,6 +26,11 @@ mklink ~/.bash_profile
 mklink ~/.bashrc
 mklink ~/.xinitrc
 mklink ~/.Xdefaults
+
+# Global gitignore
+mklink ~/.global_gitignore
+git config --global core.excludesfile ~/.gitignore_global
+
 
 # compton
 [[ -f $(command -v compton 2>/dev/null) ]] && mklink ~/.compton.conf
